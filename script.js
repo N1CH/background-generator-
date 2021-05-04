44c4d4ad90d15b55990333188a8fd3254c523728
N1CH <nicachuma@gmail.com>
2021-05-03 23:12:31 -0300

    Fixes

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
-css.textContent = body.style.background + ";";
-
 function setGradient() {
 	body.style.background = 
 	"linear-gradient(to right, " 
